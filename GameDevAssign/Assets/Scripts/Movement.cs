2989b3e7af28e8c1963d715f8821b99233518db4
Jomar <75926407+Jomar77@users.noreply.github.com>
2024-10-04 19:50:31 +1300

    Tried updating movement
    This reverts commit e1c789bf2c3e41d51ab2e5d27611fa927a71a69b.

--- GameDevAssign/Assets/Scripts/Movement.cs
+++ GameDevAssign/Assets/Scripts/Movement.cs
@@ -1,57 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Movement :
-MonoBehaviour
+public class Movement : MonoBehaviour
 {
     public float moveSpeed = 3f;
-    public float jumpHeight = 10f;
-    public bool grounded;
     private Animator anim;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
-    private AudioSource jumpSound;
-
-    // Start is called before the
 
     void Start()
     {
         anim =
         GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
-        jumpSound = GetComponent<AudioSource>();
     }
-    // Update is called once per
 
     void Update()
     {
-        anim.SetFloat("Speed",rb.velocity.y);
+        anim.SetFloat("Speed", rb.velocity.y);
+        anim.SetBool("IsRunningHor", false);
+        anim.SetBool("IsRunningVer", false);
         if
         (Input.GetKey(KeyCode.D))
         {
             sr.flipX = false;
             rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
-            anim.SetBool("Running", true);
+            anim.SetBool("IsRunningHor", true);
+            anim.SetBool("IsRunningVer", false);
         }
-        if
+        else if
         (Input.GetKey(KeyCode.A))
         {
             sr.flipX = true;
-            rb.velocity = new Vector2(-moveSpeed,rb.velocity.y);
-            anim.SetBool("Running", true);
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+            anim.SetBool("IsRunningHor", true);
+            anim.SetBool("IsRunningVer", false);
         }
-         if(Input.GetKey(KeyCode.W))
+        else if
+        (Input.GetKey(KeyCode.S))
         {
-            sr.flipY = true;
-            rb.velocity = new Vector2(moveSpeed, rb.velocity.x);
-            anim.SetBool("Running", true);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            sr.flipY = false;
+            sr.Equals("Assets/Sprites/New Piskel (2).png");
             rb.velocity = new Vector2(-moveSpeed, rb.velocity.x);
-            anim.SetBool("Running", true);
+            anim.SetBool("IsRunningHor", false);
+            anim.SetBool("IsRunningVer", true);
         }
+        else if
+        (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.x);
+            anim.SetBool("IsRunningVer", true);
+            anim.SetBool("IsRunningHor", false);
 
+        }
     }
 }
